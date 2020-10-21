e0f6d294ec5127e0477b470fa5dc2538306c9dc8
CrowBaldeYT <54008552+davidlouis101@users.noreply.github.com>
2020-10-21 19:30:46 +0200

    Making Poggit Happy

--- src/spawnondefault/Spawn.php
+++ src/spawnondefault/Spawn.php
@@ -10,7 +10,13 @@ class main extends Plugin implements Listener{
 
 	public function onEnable(){
   		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-      		$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
+      		$this->getServer()->getLogger()->info("AlwaysSpawn Plugin in noe Enabled!");
+                $this->getServer()->getLogger()->info("Autor: Teunboeke & davidlouis101");
+          	}
+
+        public function onDisable{
+      		$this->getServer()->getLogger()->info("AlwaysSpawn Plugin is now Disabled");
+                $this->getServer()->getLogger()->info("Autor: Teunboeke & davidlouis101");
           	}
             
           	public function onPlayerLogin(PlayerLoginEvent $event){  

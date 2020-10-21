ac5f7c471105b2258d73061f3075f4f951128743
CrowBaldeYT <54008552+davidlouis101@users.noreply.github.com>
2020-10-21 19:38:16 +0200

    Making Happy

--- src/spawnondefault/Spawn.php
+++ src/spawnondefault/Spawn.php
@@ -5,19 +5,20 @@ namespace spawnondefault;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\utils\TextFormat as TF;
 
 class main extends Plugin implements Listener{
 
 	public function onEnable(){
   		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-      		$this->getServer()->getLogger()->info("AlwaysSpawn Plugin in noe Enabled!");
-                $this->getServer()->getLogger()->info("Autor: Teunboeke & davidlouis101");
+      		$this->getServer()->getLogger()->info(TF::GREEN . "I been Enabled");
+                $this->getServer()->getLogger()->info(TF::AQUA . "Autor: Teunboeke & davidlouis101");
           	}
 
-        public function onDisable{
-      		$this->getServer()->getLogger()->info("AlwaysSpawn Plugin is now Disabled");
-                $this->getServer()->getLogger()->info("Autor: Teunboeke & davidlouis101");
-          	}
+        public function onDisable() {
+	        $this->getServer()->getLogger()->info(TF::GREEN . "I been Enabled");
+                $this->getServer()->getLogger()->info(TF::AQUA . "Autor: Teunboeke & davidlouis101");
+	        }
             
           	public function onPlayerLogin(PlayerLoginEvent $event){  
             		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());

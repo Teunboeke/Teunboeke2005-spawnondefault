6e36cf957da653c18038d2b9cc1c567f7c819203
Teunboeke2005 <58904370+Teunboeke@users.noreply.github.com>
2020-10-22 15:44:39 +0200

    Update main.php

--- main.php
+++ main.php
@@ -1,19 +1,27 @@
 <?php
 
-namespace Teunboeke/spawnondefault;
+namespace spawnondefault;
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\plugin\PluginBase as Plugin;
+use pocketmine\utils\TextFormat as TF;
 
 class main extends Plugin implements Listener{
+	
 	public function onEnable(){
   		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-      		$this->getServer()->getLogger()->info("AlwaysSpawn Enabled!");
+	this->getServer()->getLogger()->info(TF::GREEN . "I been Enabled");
+      		                $this->getServer()->getLogger()->info(TF::AQUA . "Autor: Teunboeke & davidlouis101");
           	}
             
-          	public function onPlayerLogin(PlayerLoginEvent $event){  
-            		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-            }
-                  
-   }                  
+          	        public function onDisable() {
+           	        $this->getServer()->getLogger()->info(TF::GREEN . "I been Disabled");
+				                $this->getServer()->getLogger()->info(TF::AQUA . "Autor: Teunboeke & davidlouis101");
+					        }
+	
+	          	public function onPlayerLogin(PlayerLoginEvent $event){  
+                        		$event->getPlayer()->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+				          
+			}       
+   }
